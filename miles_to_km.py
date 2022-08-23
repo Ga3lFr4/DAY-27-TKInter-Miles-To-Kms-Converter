e4178533f3ff0507c2f1a019b9c3eeecfbe30912
Gaël <gael.francoise@gmail.com>
2022-08-23 10:25:48 +0200

    added km to miles conversion and easy way to switch between the two conversions

--- miles_to_km.py
+++ miles_to_km.py
@@ -8,25 +8,52 @@ window.config(pady=20, padx=20)
 
 def miles_to_km():
     new_text = int(entry.get()) * 1.609
-    km_var.config(text=str(new_text))
+    converted_int_label.config(text=str(new_text))
+
+
+def km_to_miles():
+    new_text = round((int(entry.get()) / 1.609), 3)
+    converted_int_label.config(text=str(new_text))
+
+
+def km_conversion():
+    window.title("Km to Miles Converter")
+    to_convert_label.config(text="Km")
+    converted_label.config(text="Miles")
+    converted_int_label.config(text=0)
+    calc_button.config(command=km_to_miles)
+    change_conversion_button.config(text="Miles to Km", command=miles_conversion)
+
+
+def miles_conversion():
+    window.title("Miles to Km Converter")
+    to_convert_label.config(text="Miles")
+    converted_label.config(text="Km")
+    converted_int_label.config(text=0)
+    calc_button.config(command=miles_to_km)
+    change_conversion_button.config(text="Km to Miles", command=km_conversion)
+
 
 
 entry = Entry(width=10)
 entry.grid(column=1, row=0)
 
-miles_label = Label(text="Miles")
-miles_label.grid(column=2, row=0)
+to_convert_label = Label(text="Miles")
+to_convert_label.grid(column=2, row=0)
 
 is_equal = Label(text="is equal to")
 is_equal.grid(column=0, row=1)
 
-km_var = Label(text=0)
-km_var.grid(column=1, row=1)
+converted_int_label = Label(text=0)
+converted_int_label.grid(column=1, row=1)
 
-km_label = Label(text="Km")
-km_label.grid(column=2, row=1)
+converted_label = Label(text="Km")
+converted_label.grid(column=2, row=1)
 
 calc_button = Button(text="Calculate", command=miles_to_km)
 calc_button.grid(column=1, row=2)
+
+change_conversion_button = Button(text="Km to Miles", command=km_conversion)
+change_conversion_button.grid(column=0, row=2)
 
 window.mainloop()
